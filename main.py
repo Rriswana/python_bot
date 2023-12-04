451b7af0edbd0eeeb1e3eb8f126f7c66959a16ac
Riswana Basheer <riswanabasheer14@gmail.com>
2023-12-04 15:40:59 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from chatterbot import ChatBot
+from chatterbot.conversation import Statement
+from chatterbot.trainers import ChatterBotCorpusTrainer
+chatBot = ChatBot('ChatBot')
+trainer = ChatterBotCorpusTrainer(chatBot)
+
+trainer.train("chatterbot.corpus.english")
+##print(chatBot.get_response("Hello"))
+
+while True:
+    query = input(">>>")
+    print(chatBot.get_response(Statement(text=query,search_text=query)))
+
